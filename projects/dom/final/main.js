8df15513a024cfdb65b30a4a04000eac802d67dc
Lenin Compres <lenincompres@gmail.com>
2023-11-16 12:04:37 -0500

    csv

--- projects/dom/final/main.js
+++ projects/dom/final/main.js
@@ -13,32 +13,6 @@ const COLOR = {
   accent: "#46a",
 }
 
-const CSS = {
-  h: {
-    fontFamily: "serif",
-  },
-  button: {
-    backgroundColor: COLOR.accent,
-    color: COLOR.light,
-    margin: "0 0.25em",
-    fontSize: "1.2em",
-    borderColor: COLOR.accent,
-    boxShadow: "none",
-    hover: {
-      borderColor: COLOR.light,
-    },
-    __disabled: {
-      backgroundColor: "transparent",
-      borderColor: COLOR.accent,
-      color: COLOR.accent,
-      opacity: 0.6,
-    },
-    active: {
-      borderColor: COLOR.base,
-    }
-  },
-}
-
 const cardDeck = new Deck({
   margin: "0.2em",
   fontFamily: "fantasy",
@@ -51,81 +25,107 @@ const cardHand1 = new Hand();
 
 DOM.set({
   title: "Deck of Cards",
-  css: CSS,
+  css: {
+    h: {
+      fontFamily: "serif",
+    },
+    button: {
+      backgroundColor: COLOR.accent,
+      color: COLOR.light,
+      margin: "0 0.25em",
+      fontSize: "1.2em",
+      borderColor: COLOR.accent,
+      boxShadow: "none",
+      hover: {
+        borderColor: COLOR.light,
+      },
+      __disabled: {
+        backgroundColor: "transparent",
+        borderColor: COLOR.accent,
+        color: COLOR.accent,
+        opacity: 0.6,
+      },
+      active: {
+        borderColor: COLOR.base,
+      }
+    },
+  },
   color: COLOR.dark,
   display: "flex",
   flexDirection: "column",
   backgroundColor: COLOR.base,
-});
 
-DOM.set({
-  display: "flex",
-  alignItems: "center",
-  flexDirection: "column",
-  padding: "1em",
-  H1: "Deck of Cards",
-  p: "Click the cards to flip them.",
-}, "header");
-
-DOM.set({
-  margin: "0 auto 1em",
-  width: "fit-content",
   header: {
-    padding: "0.5em",
-    menu: {
-      marginTop: "1em",
-      display: "flex",
-      justifyContent: "center",
-      button: {
-        class: {
-          disabled: cardDeck._idle.as(true, false),
-        },
-        content: [{
-          text: "Flip all",
-          onclick: () => cardDeck.flipCards(),
-        }, {
-          text: "Show all",
-          onclick: () => cardDeck.flipCards(true),
-        }, {
-          text: "Hide all",
-          onclick: () => cardDeck.flipCards(false),
-        }, {
-          text: "Shuffle",
-          onclick: () => cardDeck.shuffleCards(),
-        }, {
-          text: "Random",
-          onclick: () => cardDeck.showRandom(randomInput.value),
-        }]
-      },
-      input: {
-        id: "randomInput",
-        type: "number",
-        margin: "0 -0.8em 0 .5em",
-        width: "3em",
-        min: 1,
-        max: 10,
-        value: 1,
-      },
-    }
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+    padding: "1em",
+    H1: "Deck of Cards",
+    p: "Click the cards to flip them.",
   },
-  cardDeck: cardDeck.set({
-    backgroundColor: COLOR.neutral,
-    margin: "1em auto",
-    padding: "0.5em",
-    maxWidth: "40em",
-  }),
-  cardHand1: cardHand1.set({
-    fontSize: "1.5em",
-    backgroundColor: COLOR.neutral,
-    margin: "1em auto",
-    padding: "0.5em",
-    width: "28em",
-    minHeight: "5em",
-  })
-}, "main");
 
-DOM.set({
-  textAlign: "center",
-  padding: "1em",
-  p: "Created by Lenin Compres",
-}, "footer");
+  main: {
+    margin: "0 auto 1em",
+    width: "fit-content",
+    header: {
+      padding: "0.5em",
+      menu: {
+        marginTop: "1em",
+        display: "flex",
+        justifyContent: "center",
+        button: {
+          class: {
+            disabled: cardDeck._idle.as(true, false),
+          },
+          content: [{
+            text: "Flip all",
+            onclick: () => cardDeck.flipCards(),
+          }, {
+            text: "Show all",
+            onclick: () => cardDeck.flipCards(true),
+          }, {
+            text: "Hide all",
+            onclick: () => cardDeck.flipCards(false),
+          }, {
+            text: "Shuffle",
+            onclick: () => cardDeck.shuffleCards(),
+          }, {
+            text: "Random",
+            onclick: () => cardDeck.showRandom(randomInput.value),
+          }]
+        },
+        input: {
+          id: "randomInput",
+          type: "number",
+          margin: "0 -0.8em 0 .5em",
+          width: "3em",
+          min: 1,
+          max: 10,
+          value: 1,
+        },
+      }
+    },
+    cardDeck: cardDeck.set({
+      fontSize: "1.2em",
+      backgroundColor: COLOR.neutral,
+      margin: "1em auto",
+      padding: "0.5em",
+      maxWidth: "40em",
+    }),
+    cardHand1: cardHand1.set({
+      fontSize: "1.5em",
+      backgroundColor: COLOR.neutral,
+      margin: "1em auto",
+      padding: "0.5em",
+      width: "28em",
+      minHeight: "5em",
+    })
+  },
+
+  footer: {
+    textAlign: "center",
+    padding: "1em",
+    p: "Created by Lenin Compres",
+  }
+
+});
